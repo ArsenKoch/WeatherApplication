d38de6fae0055a9359b7222a02699d25fa314851
ArsenKoch <arsenkocaran@icloud.com>
2022-09-11 21:19:13 +0300

    WeatherApp finished

---
+++ app/src/main/java/com/example/weatherapplication/DialogManager.kt
@@ -0,0 +1,41 @@
+package com.example.weatherapplication
+
+import android.app.AlertDialog
+import android.content.Context
+import android.widget.EditText
+
+object DialogManager {
+    fun locationSettingsDialog(context: Context,listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val dialog = builder.create()
+        dialog.setTitle("Enable location?")
+        dialog.setMessage("Location disabled, do you want enable location?")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE,"OK"){_,_ ->
+            listener.onClick(null)
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE,"CANCEL"){_,_ ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    fun searchByCityNameDialog(context: Context,listener: Listener){
+        val builder = AlertDialog.Builder(context)
+        val edTxtName = EditText(context)
+        builder.setView(edTxtName)
+        val dialog = builder.create()
+        dialog.setTitle("City name:")
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE,"OK"){_,_ ->
+            listener.onClick(edTxtName.text.toString())
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE,"CANCEL"){_,_ ->
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    interface Listener{
+        fun onClick(name: String?)
+    }
+}
--- app/src/main/java/com/example/weatherapplication/fragments/DaysFragment.kt
+++ app/src/main/java/com/example/weatherapplication/fragments/DaysFragment.kt
@@ -1,24 +1,50 @@
 package com.example.weatherapplication.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.weatherapplication.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.weatherapplication.MainViewModel
+import com.example.weatherapplication.adapters.WeatherAdapter
+import com.example.weatherapplication.adapters.WeatherModel
+import com.example.weatherapplication.databinding.FragmentDaysBinding
 
-class DaysFragment : Fragment() {
+class DaysFragment : Fragment(), WeatherAdapter.Listener {
+    private lateinit var adapter: WeatherAdapter
+    private lateinit var binding: FragmentDaysBinding
+    private val model: MainViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_days, container, false)
+    ): View {
+        binding = FragmentDaysBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+        model.liveDataList.observe(viewLifecycleOwner){
+            adapter.submitList(it.subList(1,it.size))
+        }
+    }
+
+    private fun init() = with(binding) {
+        adapter = WeatherAdapter(this@DaysFragment)
+        rcView.layoutManager = LinearLayoutManager(activity)
+        rcView.adapter = adapter
     }
 
     companion object {
-        @JvmStatic
         fun newInstance() = DaysFragment()
+    }
+
+    override fun onClick(item: WeatherModel) {
+        model.liveDataCurrent.value = item
+
     }
 }
